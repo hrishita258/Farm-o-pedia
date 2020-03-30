b90bf8a3209f6ba616586a26fbd62a6e769ce135
ejekanshjain <ejekanshjain@gmail.com>
2020-03-30 17:37:31 +0530

    made access token permanent

--- routes/api/logout.js
+++ routes/api/logout.js
@@ -5,20 +5,20 @@ const router = express.Router()
 
 const { RefreshToken } = require('../../models')
 
-router.get('/', (req, res) => {
-    res.status(400).json({ status: 400, message: 'Send post request to this api' })
-})
+// router.get('/', (req, res) => {
+//     res.status(400).json({ status: 400, message: 'Send post request to this api' })
+// })
 
-router.post('/', async (req, res) => {
-    const { refreshToken } = req.body
-    if (!refreshToken) res.status(400).json({ status: 400, message: 'refreshToken is required' })
-    try {
-        const user = await jwt.verify(refreshToken, process.env.JWT_REFRESH_TOKEN_SECRET)
-    } catch (err) {
-        return res.status(401).json({ status: 401, statusCode: 'failed', message: 'Invalid Refresh Token' })
-    }
-    await RefreshToken.deleteMany({ refreshToken })
-    res.status(200).json({ status: 200, statusCode: 'success', message: 'Logged out successfully' })
-})
+// router.post('/', async (req, res) => {
+//     const { refreshToken } = req.body
+//     if (!refreshToken) res.status(400).json({ status: 400, message: 'refreshToken is required' })
+//     try {
+//         const user = await jwt.verify(refreshToken, process.env.JWT_REFRESH_TOKEN_SECRET)
+//     } catch (err) {
+//         return res.status(401).json({ status: 401, statusCode: 'failed', message: 'Invalid Refresh Token' })
+//     }
+//     await RefreshToken.deleteMany({ refreshToken })
+//     res.status(200).json({ status: 200, statusCode: 'success', message: 'Logged out successfully' })
+// })
 
 module.exports = router
--- routes/api/token.js
+++ routes/api/token.js
@@ -6,26 +6,26 @@ const router = express.Router()
 
 const { RefreshToken } = require('../../models')
 
-router.get('/', (req, res) => {
-    res.status(400).json({ status: 400, message: 'Send post request to this api' })
-})
+// router.get('/', (req, res) => {
+//     res.status(400).json({ status: 400, message: 'Send post request to this api' })
+// })
 
-router.post('/', async (req, res) => {
-    const { refreshToken } = req.body
-    if (!refreshToken) res.status(400).json({ status: 400, message: 'refreshToken is required' })
-    let user
-    try {
-        user = await jwt.verify(refreshToken, process.env.JWT_REFRESH_TOKEN_SECRET)
-    } catch (err) {
-        return res.status(401).json({ status: 401, statusCode: 'failed', message: 'Invalid Refresh Token' })
-    }
-    const foundRefreshToken = await RefreshToken.findOne({ refreshToken })
-    if (!foundRefreshToken) return res.status(401).json({ status: 401, statusCode: 'failed', message: 'Login again' })
-    const accessToken = await jwt.sign({
-        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 12),
-        data: JSON.stringify(user)
-    }, process.env.JWT_ACCESS_TOKEN_SECRET)
-    res.status(200).json({ status: 200, statusCode: 'success', message: 'New access token generated successfully', accessToken, refreshToken })
-})
+// router.post('/', async (req, res) => {
+//     const { refreshToken } = req.body
+//     if (!refreshToken) res.status(400).json({ status: 400, message: 'refreshToken is required' })
+//     let user
+//     try {
+//         user = await jwt.verify(refreshToken, process.env.JWT_REFRESH_TOKEN_SECRET)
+//     } catch (err) {
+//         return res.status(401).json({ status: 401, statusCode: 'failed', message: 'Invalid Refresh Token' })
+//     }
+//     const foundRefreshToken = await RefreshToken.findOne({ refreshToken })
+//     if (!foundRefreshToken) return res.status(401).json({ status: 401, statusCode: 'failed', message: 'Login again' })
+//     const accessToken = await jwt.sign({
+//         exp: Math.floor(Date.now() / 1000) + (60 * 60 * 12),
+//         data: JSON.stringify(user)
+//     }, process.env.JWT_ACCESS_TOKEN_SECRET)
+//     res.status(200).json({ status: 200, statusCode: 'success', message: 'New access token generated successfully', accessToken, refreshToken })
+// })
 
 module.exports = router
