7cd3bfcabac2bf6a2e3342c633e0480f90c4df64
ejekanshjain <ejekanshjain@gmail.com>
2020-03-30 17:38:02 +0530

    temporarily disable api key

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 
 const router = express.Router()
 
-const { authApiKey } = require('../../middlewares')
+// const { authApiKey } = require('../../middlewares')
 
 router.use(authApiKey)
 
