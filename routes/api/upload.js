71945c2937d747811a841aad5b06e454b3daf0d9
ejekanshjain <ejekanshjain@gmail.com>
2020-04-01 20:41:42 +0530

    upload image and voice note api re done

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -67,39 +67,88 @@ router.post('/text', async (req, res) => {
     }
 })
 
+// router.post('/image', multer({ storage, fileFilter: imageFilter }).single('file'), async (req, res) => {
+//     if (!req.file) return res.status(400).json({ status: 400, statusCode: 400, message: 'Please upload a file' })
+//     if (req.fileValidationError) return res.status(400).json({ status: 400, statusCode: 'failed', message: req.fileValidationError })
+//     const quarantinedUserUpload = new QuarantinedUserUpload({
+//         _quarantinedUserId: req.user._id,
+//         uploadType: 'image',
+//         image: req.file.filename
+//     })
+//     try {
+//         await quarantinedUserUpload.save()
+//         res.status(201).json({ status: 201, statusCode: 'success', message: 'Upload Successfully' })
+//     } catch (err) {
+//         console.log(err)
+//         res.status(500).json({ status: 500, statusCode: 500, message: 'Something went wrong', error: err })
+//     }
+// })
+
+// router.post('/voicenote', multer({ storage, fileFilter: voiceNoteFilter }).single('file'), async (req, res) => {
+//     if (!req.file) return res.status(400).json({ status: 400, statusCode: 400, message: 'Please upload a file' })
+//     if (req.fileValidationError) return res.status(400).json({ status: 400, statusCode: 'failed', message: req.fileValidationError })
+//     const quarantinedUserUpload = new QuarantinedUserUpload({
+//         _quarantinedUserId: req.user._id,
+//         uploadType: 'voiceNote',
+//         voiceNote: req.file.filename
+//     })
+//     try {
+//         await quarantinedUserUpload.save()
+//         res.status(201).json({ status: 201, statusCode: 'success', message: 'Upload Successfully' })
+//     } catch (err) {
+//         console.log(err)
+//         res.status(500).json({ status: 500, statusCode: 500, message: 'Something went wrong', error: err })
+//     }
+// })
+
 router.post('/image', multer({ storage, fileFilter: imageFilter }).single('file'), async (req, res) => {
-    console.log(req.file)
-    if (!req.file) return res.status(400).json({ status: 400, statusCode: 400, message: 'Please upload a file' })
-    if (req.fileValidationError) return res.status(400).json({ status: 400, statusCode: 'failed', message: req.fileValidationError })
-    const quarantinedUserUpload = new QuarantinedUserUpload({
-        _quarantinedUserId: req.user._id,
-        uploadType: 'image',
-        image: req.file.filename
+    let { file } = req.body
+    if (!file) return res.status(400).json({ status: 400, statusCode: 'failed', message: 'Please upload a file' })
+    let base64Image = file.split(';base64,').pop()
+    let fileName = `${req.user._id + '-' + randomString(8) + Date.now()}.bmp`
+    fs.writeFile(`uploads/${fileName}`, base64Image, { encoding: 'base64' }, async err => {
+        if (err) {
+            console.log(err)
+            return res.status(500).json({ status: 500, statusCode: 'failed', message: 'Something wenrt wrong', error: err })
+        }
+        const quarantinedUserUpload = new QuarantinedUserUpload({
+            _quarantinedUserId: req.user._id,
+            uploadType: 'image',
+            image: fileName
+        })
+        try {
+            await quarantinedUserUpload.save()
+            res.status(201).json({ status: 201, statusCode: 'success', message: 'Upload Successfully' })
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({ status: 500, statusCode: 500, message: 'Something went wrong', error: err })
+        }
     })
-    try {
-        await quarantinedUserUpload.save()
-        res.status(201).json({ status: 201, statusCode: 'success', message: 'Upload Successfully' })
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({ status: 500, statusCode: 500, message: 'Something went wrong', error: err })
-    }
 })
 
 router.post('/voicenote', multer({ storage, fileFilter: voiceNoteFilter }).single('file'), async (req, res) => {
-    if (!req.file) return res.status(400).json({ status: 400, statusCode: 400, message: 'Please upload a file' })
-    if (req.fileValidationError) return res.status(400).json({ status: 400, statusCode: 'failed', message: req.fileValidationError })
-    const quarantinedUserUpload = new QuarantinedUserUpload({
-        _quarantinedUserId: req.user._id,
-        uploadType: 'voiceNote',
-        voiceNote: req.file.filename
+    let { file } = req.body
+    if (!file) return res.status(400).json({ status: 400, statusCode: 'failed', message: 'Please upload a file' })
+    let base64Image = file.split(';base64,').pop()
+    let fileName = `${req.user._id + '-' + randomString(8) + Date.now()}.3gp`
+    fs.writeFile(`uploads/${fileName}`, base64Image, { encoding: 'base64' }, function (err) {
+        if (err) {
+            console.log(err)
+            return res.status(500).json({ status: 500, statusCode: 'failed', message: 'Something wenrt wrong', error: err })
+        }
+        const quarantinedUserUpload = new QuarantinedUserUpload({
+            _quarantinedUserId: req.user._id,
+            uploadType: 'voiceNote',
+            voiceNote: fileName
+        })
+        try {
+            await quarantinedUserUpload.save()
+            res.status(201).json({ status: 201, statusCode: 'success', message: 'Upload Successfully' })
+        } catch (err) {
+            console.log(err)
+            res.status(500).json({ status: 500, statusCode: 500, message: 'Something went wrong', error: err })
+        }
     })
-    try {
-        await quarantinedUserUpload.save()
-        res.status(201).json({ status: 201, statusCode: 'success', message: 'Upload Successfully' })
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({ status: 500, statusCode: 500, message: 'Something went wrong', error: err })
-    }
 })
 
 module.exports = router
