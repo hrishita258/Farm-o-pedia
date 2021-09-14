d701dd766185025126fa2b4445e8075f7e83a0af
thimanshu3 <himanshu.zeddlabs@gmail.com>
2021-09-14 17:14:19 +0530

    chatbot integrated

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -9,38 +9,96 @@ function getRandomNumberBetween(min,max){
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
+const findAverageAge = (arr) => {
+   const { length } = arr;
+   return arr.reduce((acc, val) => {
+      return acc + (val.value/length);
+   }, 0);
+};
+
+const getFertilizersAndCrop = async(arr , type) => {
+    if(type === 'fertilizers'){
+          const browser = await puppeteer.launch({ headless: false, devtools: true })
+        const page = await browser.newPage()
+        await page.goto('https://harvestify.herokuapp.com/fertilizer')
+
+        await page.click('#Nitrogen')
+        await page.keyboard.type('20')
+
+        await page.click('#Phosphorous')
+        await page.keyboard.type('20')
+
+        await page.click('#Pottasium')
+        await page.keyboard.type('20')
+
+        await page.select('select#crop', 'rice')
+
+        await page.click('.btn-info')
+        const html = await page.content()
+    }else{
+        const browser = await puppeteer.launch({ headless: false, devtools: true })
+        const page = await browser.newPage()
+        await page.goto('https://harvestify.herokuapp.com/crop-recommend')
+
+        await page.click('#Nitrogen')
+        await page.keyboard.type('20')
+
+        await page.click('#Phosphorous')
+        await page.keyboard.type('20')
+
+        await page.click('#Pottasium')
+        await page.keyboard.type('20')
+
+        await page.click('#ph')
+        await page.keyboard.type('20')
+
+        await page.click('#Rainfall')
+        await page.keyboard.type('20')
+
+        await page.select('select#sts', 'Rajasthan')
+
+        await page.select('select#state', 'Udaipur')
+
+        await page.click('.btn-info')
+        const html = await page.content()
+    }
+
+}
+
 
 router.get('/', async(req, res) => {
     
-
-    res.render('admin', { price: "something" })
+    const data = await SensorData.find().sort({ date_time: 'asc' })
+    const phdata = data.filter(a => a.sensor === 'ph')
+const tempdata = data.filter(a => a.sensor === 'temperature')
+    res.render('admin', {phvalue: findAverageAge(phdata), tempValue: findAverageAge(tempdata)+20 })
 })
 
 router.get('/generate', async(req, res) => {
-//     var i,
-//   j,
-//   temporary,
-//   count = 0,
-//   chunk = 2000
-// for (i = 0, j = data.length; i < j; i += chunk) {
-//     count++
-//   temporary = data.slice(i, i + chunk)
-//   console.log(temporary)
-//   try {
-// //     console.log('started')
-// //     console.log(await SensorData.insertMany(temporary.map((doc) => ({
-// //       sensor: 'temperature', 
-// //       value: doc.sensor_value,
-// //       date_time:(`2021-09-0${count+1}`)
-// //   }))))
-//   } catch (error) {
-//       console.log(error)
-//   }
+    var i,
+  j,
+  temporary,
+  count = 0,
+  chunk = 20
+for (i = 0, j = 140; i < j; i += chunk) {
+    count++
+  temporary = data.slice(i, i + chunk)
+  console.log(temporary)
+  try {
+    console.log('started')
+    console.log(await SensorData.insertMany(temporary.map((doc) => ({
+      sensor: 'temperature', 
+      value: doc.sensor_value,
+      date_time:(`2021-09-0${count+1}`)
+  }))))
+  } catch (error) {
+      console.log(error)
+  }
   
 
-// }
+}
 
-for(i=0; i<1000; i++){
+for(i=0; i<100; i++){
     console.log(await SensorData.create({
         sensor: 'ph',
         value: getRandomNumberBetween(2.5, 5.5),
